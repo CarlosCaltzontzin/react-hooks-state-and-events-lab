f8d95be5511524accc4c94f322a711a4161914a4
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-11-09 13:33:53 -0600

    completed phase2 react state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // State variable to track dark mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Function to handle the dark mode toggle
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  // Use the isDarkMode state to conditionally apply the 'dark' or 'light' class
+  const appClass = isDarkMode ? 'App dark' : 'App light';
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={toggleDarkMode}>{isDarkMode ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
